8044980c20fb59e768f147d62f3886589ca855a8
Godwin-ulaka <103589441+Godwin-ulaka@users.noreply.github.com>
2022-07-01 09:34:58 +0100

    my website portfolio

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/******************* typing animation **************************** */
+var typed = new typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Ad Manager"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
